a8a6c245571a142cdf4b5652e3a12248e4ba03bf
Elena Magay <elenamagay1985@gmail.com>
2022-10-31 11:08:46 -0400

    created a private footer, icon component added to package.json

--- src/components/Footer/FooterPrivate.js
+++ src/components/Footer/FooterPrivate.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { Container, Box, Link } from "@mui/material";
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 const FooterPrivate = () => {
-  return <div>FooterPrivate</div>;
+  return <footer>
+  <Box 
+    bgcolor="text.secondary" 
+    color="white" 
+    textAlign="center" 
+    pt="5" 
+    pb="0">
+    <Container maxWidth="lg">
+      <Link href="https://github.com/Code-the-Dream-School/MyKitchenApp-front">
+        <GitHubIcon />
+      </Link>
+      My Kitchen App &#169; {new Date().getFullYear()}
+    </Container>
+  </Box>
+</footer>
 };
 
 export default FooterPrivate;
