c7f6ab8a72ecd1a1703b7ad8e6e071e575a8c141
Elena Magay <elenamagay1985@gmail.com>
2022-10-31 11:27:55 -0400

    fixed issue with sticking private footer to the bottom

--- src/components/Footer/FooterPrivate.js
+++ src/components/Footer/FooterPrivate.js
@@ -4,19 +4,20 @@ import GitHubIcon from '@mui/icons-material/GitHub';
 
 const FooterPrivate = () => {
   return <footer>
+  
   <Box 
     bgcolor="text.secondary" 
     color="white" 
     textAlign="center" 
-    pt="5" 
-    pb="0">
-    <Container maxWidth="lg">
+    sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}>
+    <Container maxWidth="lg" >
       <Link href="https://github.com/Code-the-Dream-School/MyKitchenApp-front">
         <GitHubIcon />
       </Link>
       My Kitchen App &#169; {new Date().getFullYear()}
     </Container>
   </Box>
+  
 </footer>
 };
 
