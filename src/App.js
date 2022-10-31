a5f069e98b839c38ff346e6a8c392ab844d4e401
XeniyaDob <xeniya.dobrogorskaya@gmail.com>
2022-10-31 09:54:03 -0400

    Responsive NavBar added, without routs yet

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import SignIn from "./Pages/SignIn/SignIn";
 import "./App.css";
+import ResponsiveAppBar from "./components/Navbar/ResponsiveAppBar";
 
 function App() {
   return (
     <div className="App">
+      <ResponsiveAppBar />
       <SignIn />
     </div>
   );
