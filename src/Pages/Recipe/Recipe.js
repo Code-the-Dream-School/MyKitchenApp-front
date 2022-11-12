fcd73013092c10a1be752b0a4b94e7252b268978
XeniyaDob <xeniya.dobrogorskaya@gmail.com>
2022-11-12 06:54:42 -0500

    Recipe page folder and files created

---
+++ src/Pages/Recipe/Recipe.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+const Recipe = () => {
+  return <div>Recipe</div>;
+};
+
+export default Recipe;
