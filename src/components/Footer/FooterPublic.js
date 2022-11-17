622286e8aa9b87b0b77d9c877f122b9170f38d47
Elena Magay <elenamagay1985@gmail.com>
2022-11-17 18:37:48 -0500

    fixed the bug, added a link to Spoonacular

--- src/components/Footer/FooterPublic.js
+++ src/components/Footer/FooterPublic.js
@@ -1,14 +1,34 @@
 import React from "react";
-import { Container, Box } from "@mui/material";
-
+import { Container, Box, Grid } from "@mui/material";
+import Link from "@mui/material/Link";
 const FooterPublic = () => {
-  return <footer>
-  <Box bgcolor="text.secondary" color="white" textAlign="center" pt="5" pb="0">
-    <Container maxWidth="lg">      
-      My Kitchen App &#169; {new Date().getFullYear()}
-    </Container>
-  </Box>
-</footer>
+  return (
+    <footer>
+      <Box
+        bgcolor="text.secondary"
+        color="white"
+        textAlign="center"
+        sx={{ 
+          position: "fixed", 
+          bottom: 0, 
+          left: 0, 
+          right: 0 
+        }}
+      >
+        <Container maxWidth="lg">
+          <Grid>
+            My Kitchen App &#169; {new Date().getFullYear()} 
+          </Grid>
+          <Grid>
+            Powered by {" "}
+            <Link color="inherit" underline="hover" href="https://spoonacular.com/food-api">
+              Spoonacular API
+            </Link>           
+          </Grid>
+        </Container>
+      </Box>
+    </footer>
+  );
 };
 
 export default FooterPublic;
