27f89fb076d864cc0b9bf4603709dd57e7100e58
Elena Magay <elenamagay1985@gmail.com>
2022-11-17 18:39:48 -0500

    refactored imports

--- src/components/Footer/FooterPublic.js
+++ src/components/Footer/FooterPublic.js
@@ -1,6 +1,9 @@
 import React from "react";
-import { Container, Box, Grid } from "@mui/material";
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid"
 import Link from "@mui/material/Link";
+
 const FooterPublic = () => {
   return (
     <footer>
