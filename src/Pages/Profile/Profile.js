c4e609d1bd6ab363b5976917da8930fb6c587ef7
Nur <nuriyealp66@gmail.com>
2022-11-22 21:49:10 -0800

    ability to change password

--- src/Pages/Profile/Profile.js
+++ src/Pages/Profile/Profile.js
@@ -4,15 +4,60 @@ import Box from "@mui/material/Box";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import TextField from "@mui/material/TextField";
 import Typography from "@mui/material/Typography";
+import axios from "axios";
 
 const Profile = () => {
   const [isEdittingAccount, setIsEdittingAccount] = useState(false);
+  const [isEdittingPassword, setIsEdittingPassword] = useState(false);
+
+  const [password, setPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
 
   const user = JSON.parse(localStorage.getItem("myKitchenAppUser"));
 
-  const handleSubmit = (event) => {
+  const handleEditAccountSubmit = (event) => {
     event.preventDefault();
     setIsEdittingAccount(false);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+    // if (event.target.value.length < 8) {
+    //   setIsPasswordInvalid(true);
+    //   setInvalidPasswordMessage("Password must be at least 8 characters.");
+    // } else {
+    //   setIsPasswordInvalid(false);
+    //   setInvalidPasswordMessage("");
+    // }
+  };
+  const handleNewPasswordChange = (event) => {
+    setNewPassword(event.target.value);
+  };
+
+  const handleEditPasswordSubmit = (event) => {
+    event.preventDefault();
+    const url = "/api/v1/auth/changePassword";
+    const data = {
+      password: password,
+      newPassword: newPassword,
+    };
+
+    const token = localStorage.getItem("myKitchenAppToken");
+
+    axios
+      .patch(url, data, {
+        headers: {
+          Authorization: "Bearer " + token,
+          "Content-Type": "application/json",
+        },
+      })
+      .then((response) => {
+        console.log(response);
+        setIsEdittingPassword(false);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
   return (
@@ -25,36 +70,17 @@ const Profile = () => {
         justifyContent: "space-evenly",
       }}
     >
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-        }}
-      >
-        <AccountCircleIcon sx={{ fontSize: 120, color: "grey" }} />
-        <Button
-          variant="outlined"
-          sx={{
-            display: "inline",
-            height: "50px",
-            backgroundColor: "black",
-            color: "white",
-            "&:hover": {
-              backgroundColor: "#5a5a5a",
-            },
-          }}
-        >
-          Upload
-        </Button>
-      </Box>
-      <Box>
+      <AccountCircleIcon
+        sx={{ fontSize: 120, color: "grey", alignSelf: "start", flex: 1 }}
+      />
+      <Box sx={{ flex: 4 }}>
         <Typography component="h1" variant="h3">
           Profile
         </Typography>
         {isEdittingAccount ? (
           <Box
             component="form"
-            onSubmit={handleSubmit}
+            onSubmit={handleEditAccountSubmit}
             noValidate
             sx={{ mt: 1 }}
           >
@@ -99,6 +125,64 @@ const Profile = () => {
               Save
             </Button>
           </Box>
+        ) : isEdittingPassword ? (
+          <Box
+            component="form"
+            onSubmit={handleEditPasswordSubmit}
+            noValidate
+            sx={{ mt: 1 }}
+          >
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              sx={{ width: "400px", display: "block" }}
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              onChange={handlePasswordChange}
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              sx={{ width: "400px", display: "block" }}
+              id="new-password"
+              label="New Password"
+              name="new-password"
+              type="password"
+              onChange={handleNewPasswordChange}
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              sx={{ width: "400px", display: "block" }}
+              id="confirm-password"
+              label="Confirm Password"
+              name="confirm-password"
+              type="password"
+            />
+            <Button
+              type="submit"
+              variant="outlined"
+              sx={{
+                mt: 3,
+                mb: 2,
+                display: "inline",
+                height: "50px",
+                backgroundColor: "black",
+                color: "white",
+                "&:hover": {
+                  backgroundColor: "#5a5a5a",
+                },
+              }}
+            >
+              Save
+            </Button>
+          </Box>
         ) : (
           <Box>
             <Typography sx={{ width: "400px" }}>Name: {user.name}</Typography>
@@ -119,6 +203,24 @@ const Profile = () => {
               }}
             >
               Edit Account
+            </Button>
+            <Button
+              onClick={() => setIsEdittingPassword(true)}
+              variant="outlined"
+              sx={{
+                mt: 3,
+                mb: 2,
+                ml: 2,
+                display: "inline",
+                height: "50px",
+                backgroundColor: "black",
+                color: "white",
+                "&:hover": {
+                  backgroundColor: "#5a5a5a",
+                },
+              }}
+            >
+              Change Password
             </Button>
           </Box>
         )}
