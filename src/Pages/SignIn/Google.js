cc4f5dce9d92f4198ccf02ccf7ba0df31d14d5f4
dahlak76 <88516807+Dahlak76@users.noreply.github.com>
2022-11-23 16:06:21 -0600

    checked merge conflict/ changed from class to functional/ changed div to MUI button

--- src/Pages/SignIn/Google.js
+++ src/Pages/SignIn/Google.js
@@ -1,75 +1,116 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom';
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
 
-const withRouter = (Component) => {
-  const Wrapper = (props) => {
-    const navigate = useNavigate();
-    
-    return (
-      <Component
-        navigate={navigate}
-        {...props}
-        />
-    );
+const Google = () => {
+  const navigate = useNavigate();
+
+  const handleGoogleSignIn = () => {
+    const url = "/api/v1/auth/google";
+    window  
+      .fetch(url, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      .then((response) => {
+        console.log(response);
+        navigate("/dashboard");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
-  
-  return Wrapper;
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <Button
+        type="submit"
+        fullWidth
+        variant="contained"
+        sx={{ mt: 3, mb: 2 }}
+        onClick={handleGoogleSignIn}
+      >
+        Sign In with Google
+      </Button>
+    </Container>
+  );
 };
 
-class Google extends React.Component {
+export default Google;
 
-  constructor(props) {
-    super(props)
-    window.customCallback = this.customCallback.bind(this)
-    this.customCallback=this.customCallback.bind(this);
-  }
+
+// const withRouter = (Component) => {
+//   const Wrapper = (props) => {
+//     const navigate = useNavigate();
+    
+//     return (
+//       <Component
+//         navigate={navigate}
+//         {...props}
+//         />
+//     );
+//   };
   
-  customCallback(response) {
-    localStorage.setItem("token", response.credential)
-    this.props.navigate('/dashboard')
-  }
+//   return Wrapper;
+// };
+
+// class Google extends React.Component {
+
+//   constructor(props) {
+//     super(props)
+//     window.customCallback = this.customCallback.bind(this)
+//     this.customCallback=this.customCallback.bind(this);
+//   }
   
-    componentDidMount(){
-        const script = document.createElement('script');
-        script.id = 'google-script'
-        script.src = "https://accounts.google.com/gsi/client" ;
-        script.async = true;
-        script.defer = true;
+//   customCallback(response) {
+//     localStorage.setItem("myKitchenAppToken", response.credential)
+//     this.props.navigate('/dashboard')
+//   }
+  
+//     componentDidMount(){
+//         const script = document.createElement('script');
+//         script.id = 'google-script'
+//         script.src = "https://accounts.google.com/gsi/client" ;
+//         script.async = true;
+//         script.defer = true;
       
-        document.body.appendChild(script);
-    }
+//         document.body.appendChild(script);
+//     }
 
-    componentWillUnmount(){
-        const googleScript = document.getElementById('google-script')
-        document.body.removeChild(googleScript)
-    }
+//     componentWillUnmount(){
+//         const googleScript = document.getElementById('google-script')
+//         document.body.removeChild(googleScript)
+//     }
  
   
 
-  render() {
-    return (
-      <div>
-        <div>click here to sign in with google!!!!!</div>
+//   render() {
+//     return (
+//       <Container>
+//         <Button>Sign in with Google!!!!!</Button>
             
-      <div
-        id='g_id_onload'
-        data-client_id="791096127622-ua9vefrs6j9bnrembfbmsam2sq5e0t7j.apps.googleusercontent.com"
-        data-callback='customCallback'
-        data-auto_prompt="false"
-      />
-      <div 
-        className="g_id_signin"
-        data-type="standard"
-        data-size="large"
-        data-theme="outline"
-        data-text="sign_in_with"
-        data-shape="rectangular"
-        data-logo_alignment="left">
-      </div>
-      </div>
-    )
-  }
-}
-export default withRouter(Google)
+//       <>
+//         id='g_id_onload'
+//         data-client_id="791096127622-ua9vefrs6j9bnrembfbmsam2sq5e0t7j.apps.googleusercontent.com"
+//         data-callback='customCallback'
+//         data-auto_prompt="false"
+//       </>
+//       <div 
+//         className="g_id_signin"
+//         data-type="standard"
+//         data-size="large"
+//         data-theme="outline"
+//         data-text="sign_in_with"
+//         data-shape="rectangular"
+//         data-logo_alignment="left">
+//       </div>
+//       </Container>
+//     )
+//   }
+// }
+// export default withRouter(Google)
 
 
