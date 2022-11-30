2c264ab411160a6d00c205216359fd16b0c15c8d
dahlak76 <88516807+Dahlak76@users.noreply.github.com>
2022-11-29 21:03:46 -0600

    fixed google bug

--- src/Pages/SignIn/Google.js
+++ src/Pages/SignIn/Google.js
@@ -1,116 +1,120 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom';
-import Button from "@mui/material/Button";
-import Container from "@mui/material/Container";
+import jwt_decode from 'jwt-decode'
+// import Button from "@mui/material/Button";
+// import Container from "@mui/material/Container";
 
-const Google = () => {
-  const navigate = useNavigate();
-
-  const handleGoogleSignIn = () => {
-    const url = "/api/v1/auth/google";
-    window  
-      .fetch(url, {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      })
-      .then((response) => {
-        console.log(response);
-        navigate("/dashboard");
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+const withRouter = (Component) => {
+  const Wrapper = (props) => {
+    const navigate = useNavigate();
+    
+    return (
+      <Component
+        navigate={navigate}
+        {...props}
+        />
+    );
   };
-
-  return (
-    <Container component="main" maxWidth="xs">
-      <Button
-        type="submit"
-        fullWidth
-        variant="contained"
-        sx={{ mt: 3, mb: 2 }}
-        onClick={handleGoogleSignIn}
-      >
-        Sign In with Google
-      </Button>
-    </Container>
-  );
+  
+  return Wrapper;
 };
 
-export default Google;
+class Google extends React.Component {
 
-
-// const withRouter = (Component) => {
-//   const Wrapper = (props) => {
-//     const navigate = useNavigate();
-    
-//     return (
-//       <Component
-//         navigate={navigate}
-//         {...props}
-//         />
-//     );
-//   };
+  constructor(props) {
+    super(props)
+    window.customCallback = this.customCallback.bind(this)
+    this.customCallback=this.customCallback.bind(this);
+  }
   
-//   return Wrapper;
-// };
-
-// class Google extends React.Component {
-
-//   constructor(props) {
-//     super(props)
-//     window.customCallback = this.customCallback.bind(this)
-//     this.customCallback=this.customCallback.bind(this);
-//   }
+  customCallback(response) {
+    localStorage.setItem("myKitchenAppToken", response.credential)
+    const payload = jwt_decode(response.credential)
+    const user = JSON.stringify( {name: payload.name, email: payload.email, type: 'google'})
+    console.log(user, 'user....')
+    localStorage.setItem('myKitchenAppUser', user)
+    console.log(jwt_decode(response.credential), 'jwtDecode.....')
+    this.props.navigate('/dashboard')
+  }
   
-//   customCallback(response) {
-//     localStorage.setItem("myKitchenAppToken", response.credential)
-//     this.props.navigate('/dashboard')
-//   }
-  
-//     componentDidMount(){
-//         const script = document.createElement('script');
-//         script.id = 'google-script'
-//         script.src = "https://accounts.google.com/gsi/client" ;
-//         script.async = true;
-//         script.defer = true;
+    componentDidMount(){
+        const script = document.createElement('script');
+        script.id = 'google-script'
+        script.src = "https://accounts.google.com/gsi/client" ;
+        script.async = true;
+        script.defer = true;
       
-//         document.body.appendChild(script);
-//     }
+        document.body.appendChild(script);
+    }
 
-//     componentWillUnmount(){
-//         const googleScript = document.getElementById('google-script')
-//         document.body.removeChild(googleScript)
-//     }
+    componentWillUnmount(){
+        const googleScript = document.getElementById('google-script')
+        document.body.removeChild(googleScript)
+    }
  
   
 
-//   render() {
-//     return (
-//       <Container>
-//         <Button>Sign in with Google!!!!!</Button>
+  render() {
+    return (
+      <div>
+        <div>Sign in with Google!!!!!</div>
             
-//       <>
-//         id='g_id_onload'
-//         data-client_id="791096127622-ua9vefrs6j9bnrembfbmsam2sq5e0t7j.apps.googleusercontent.com"
-//         data-callback='customCallback'
-//         data-auto_prompt="false"
-//       </>
-//       <div 
-//         className="g_id_signin"
-//         data-type="standard"
-//         data-size="large"
-//         data-theme="outline"
-//         data-text="sign_in_with"
-//         data-shape="rectangular"
-//         data-logo_alignment="left">
-//       </div>
-//       </Container>
-//     )
-//   }
-// }
-// export default withRouter(Google)
+        <div
+        id='g_id_onload'
+        data-client_id="791096127622-ua9vefrs6j9bnrembfbmsam2sq5e0t7j.apps.googleusercontent.com"
+        data-callback='customCallback'
+        data-auto_prompt="false"
+      />
+      <div 
+        className="g_id_signin"
+        data-type="standard"
+        data-size="large"
+        data-theme="outline"
+        data-text="sign_in_with"
+        data-shape="rectangular"
+        data-logo_alignment="left">
+      </div>
+      </div>
+    )
+  }
+}
+export default withRouter(Google)
 
 
+// const Google = () => {
+//   const navigate = useNavigate();
+
+//   const handleGoogleSignIn = () => {
+//     const url = "/api/v1/auth/google";
+//     window  
+//       .fetch(url, {
+//         method: "GET",
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//       })
+//       .then((response) => {
+//         console.log(response);
+//         navigate("/dashboard");
+//       })
+//       .catch((error) => {
+//         console.log(error);
+//       });
+//   };
+
+//   return (
+//     <Container component="main" maxWidth="xs">
+//       <Button
+//         type="submit"
+//         fullWidth
+//         variant="contained"
+//         sx={{ mt: 3, mb: 2 }}
+//         onClick={handleGoogleSignIn}
+//       >
+//         Sign In with Google
+//       </Button>
+//     </Container>
+//   );
+// };
+
+// export default Google;
