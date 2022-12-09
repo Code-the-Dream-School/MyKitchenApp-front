161f0ea85b4dc4d891fa8f856868621352eb30b2
Elena Magay <elenamagay1985@gmail.com>
2022-12-09 03:59:30 -0500

    deleted unused component

--- src/components/Footer/FooterPrivate.js
+++ src/components/Footer/FooterPrivate.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
 import Link from "@mui/material/Link";
 import Grid from "@mui/material/Grid";
